6e90a3369a3e2d09c6bfe35020d8ec66a206b0b0
Allyson Reyes <areyes986@gmail.com>
2019-10-29 19:02:42 -0700

    added more files

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+vfunction askName() {
+    var username = prompt('Hello! What is your name?');
+
+    return '<h3>' + 'Welcome to Ally\'s Lab project, ' + username + '!' + '</h3>';
+}
+
+// userName = userName.toLocaleLowerCase();
+// if (userLike === 'yes' || userLike === 'y') {
+//     console.log('I like it too');
+// }   else {
+//     console.log('you shoul check it out!');
+// }
